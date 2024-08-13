27ba07dbffff2875a1a832cbb14372f429bcffb0
Yoda-Man <trustaldo@gmail.com>
2024-08-13 04:29:08 +0200

    Add Notes For Ubuntu
    Add Notes For Ubuntu

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.0.3
+
+* Add Notes For Ubuntu
+
 ## 0.0.2
 
 * Add documentation
---
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/home/ind/snap/flutter/common/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/ind/Documents/Mobile/atomic_webview/atomic_webview"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=0.0.3"
+export "FLUTTER_BUILD_NUMBER=0.0.3"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
