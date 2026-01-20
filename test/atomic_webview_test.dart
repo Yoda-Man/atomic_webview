6dc0e0f6f38dbedd1981e94b3561d444acaad32f
Yoda-Man <trustaldo@gmail.com>
2026-01-20 19:35:42 +0200

    feat: Add test for `WebviewWindow.create` ensuring it returns a `WebviewImpl` with the correct ID.

--- test/atomic_webview_test.dart
+++ test/atomic_webview_test.dart
@@ -1,18 +1,33 @@
-import 'package:flutter_test/flutter_test.dart';
+import 'package:atomic_webview/webview_desktop/src/webview_impl.dart';
+import 'package:atomic_webview/webview_desktop/webview_desktop_app.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  const MethodChannel channel = MethodChannel('webview_window');
-
   TestWidgetsFlutterBinding.ensureInitialized();
 
-  setUp(() {
-    channel.setMockMethodCallHandler((MethodCall methodCall) async {
-      return '42';
-    });
-  });
+  const MethodChannel channel = MethodChannel('webview_window');
 
-  tearDown(() {
-    channel.setMockMethodCallHandler(null);
+  group('Atomic Webview Tests', () {
+    setUp(() {
+      TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+          .setMockMethodCallHandler(channel, (MethodCall methodCall) async {
+            if (methodCall.method == 'create') {
+              return 42;
+            }
+            return null;
+          });
+    });
+
+    tearDown(() {
+      TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+          .setMockMethodCallHandler(channel, null);
+    });
+
+    test('WebviewWindow.create returns a Webview with correct ID', () async {
+      final webview = await WebviewWindow.create();
+      expect(webview, isA<WebviewImpl>());
+      expect((webview as WebviewImpl).viewId, 42);
+    });
   });
 }
